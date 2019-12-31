ebbca13ee420430f5371fe3360ac5a445696bd9b
Janaka Jati Lasmana <janakajati21@gmail.com>
2019-12-31 12:34:22 +0700

    slicing

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,20 @@
+    <!--  
+    JavaScripts
+    =============================================
+    -->
+    <script src={{ asset("/lib/jquery/dist/jquery.js") }}></script>
+    <script src={{ asset("/lib/bootstrap/dist/js/bootstrap.min.js") }}></script>
+    <script src={{ asset("/lib/wow/dist/wow.js") }}></script>
+    <script src={{ asset("/lib/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js") }}></script>
+    <script src={{ asset("/lib/isotope/dist/isotope.pkgd.js") }}></script>
+    <script src={{ asset("/lib/imagesloaded/imagesloaded.pkgd.js") }}></script>
+    <script src={{ asset("/lib/flexslider/jquery.flexslider.js") }}></script>
+    <script src={{ asset("/lib/owl.carousel/dist/owl.carousel.min.js") }}></script>
+    <script src={{ asset("/lib/smoothscroll.js") }}></script>
+    <script src={{ asset("/lib/magnific-popup/dist/jquery.magnific-popup.js") }}></script>
+    <script src={{ asset("/lib/simple-text-rotator/jquery.simple-text-rotator.min.js") }}></script>
+    <script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDK2Axt8xiFYMBMDwwG1XzBQvEbYpzCvFU"></script>
+    <script src={{ asset("/js/plugins.js") }}></script>
+    <script src={{ asset("/js/main.js") }}></script>
+  </body>
+</html>
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-custom navbar-transparent navbar-fixed-top one-page" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.html">KatanyaJati</a>
+    </div>
+    <div class="collapse navbar-collapse" id="custom-collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#totop">Home</a></li>
+        <li><a class="section-scroll" href="#services">Tentang Saya</a></li>
+        <li><a class="section-scroll" href="#works">Projects</a></li>
+        <li><a class="section-scroll" href="#alt-features">Keahlian</a></li>
+        <li><a class="section-scroll" href="#team">Team</a></li>
+        <li><a class="section-scroll" href="#pricing">Prices</a></li>
+        <li><a class="section-scroll" href="#news">News</a></li>
+        <li><a class="section-scroll" href="#contact">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
